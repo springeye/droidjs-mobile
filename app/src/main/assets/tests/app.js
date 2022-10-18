da8bb4df4a984f68dcc48bf0164a9a1753e68cd7
springeye <henjue@gmail.com>
2022-10-18 12:29:03 +0800

    update

--- app/src/main/assets/tests/app.js
+++ app/src/main/assets/tests/app.js
@@ -1,7 +1,7 @@
 //app.getApkInfo("aaa",{
 //    get:['aaa']
 //})
-console.log('==================>',app.getPackageName('口袋奇兵'));
+console.log('获取包名测试==================>',app.getPackageName('口袋奇兵'));
 //app.launch('com.rivergame.ap.worldbattle')
-let node=ui.findByText("test_button");
-node.click();
+//let node=ui.findByText("test_button");
+//node.click();
